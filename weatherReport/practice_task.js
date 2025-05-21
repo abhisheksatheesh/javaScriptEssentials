e1727c8b41d1f2c8c016b276c9b747c7883d0f4e
K S Abhishek <abhisheksatheesh92@gmail.com>
2025-05-21 19:48:22 +0530

    practice task work out

---
+++ weatherReport/practice_task.js
@@ -0,0 +1,22 @@
+function showweatherDetails(event) {
+  event.preventDefault();
+  let lat = document.getElementById("lat").value;
+  let lon = document.getElementById("lon").value;
+  const apiKey = "ada6f1afb80a48fb8f0ee4e33a9d497c";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherInfo = document.getElementById("weatherInfo");
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch((error) => {
+      console.error("Error fetching weather:", error);
+      const weatherInfo = document.getElementById("weatherInfo");
+      weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    });
+}
+
+document.getElementById("weatherForm").addEventListener("submit", showweatherDetails);
