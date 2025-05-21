029da51104660b9836168817a2017e408ea960d0
K S Abhishek <abhisheksatheesh92@gmail.com>
2025-05-21 17:43:58 +0530

    Update weather_report.js

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,7 +1,7 @@
 function showweatherDetails(event) {
   event.preventDefault();
   const city = document.getElementById("city").value;
-  const apiKey = "ada6f1afb80a48fb8f0ee4e33a9d497c"; // Replace 'YOUR_API_KEY' with your actual API key
+  const apiKey = ""; // Replace 'YOUR_API_KEY' with your actual API key
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   fetch(apiUrl)
     .then((response) => response.json())
